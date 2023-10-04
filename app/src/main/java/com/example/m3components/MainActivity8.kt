5fee99a8f3bd8404911f9771964c8aaa8b262cda
SreeragTV99 <sreeragtv03@gmail.com>
2023-10-04 15:48:33 +0530

    Added LinearProgressIndicator before loading the WebView

--- app/src/main/java/com/example/m3components/MainActivity8.kt
+++ app/src/main/java/com/example/m3components/MainActivity8.kt
@@ -1,21 +1,56 @@
 package com.example.m3components
 
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import com.google.android.material.progressindicator.LinearProgressIndicator
 
 class MainActivity8 : AppCompatActivity() {
     private lateinit var webView: WebView
+    lateinit var ProgressIndicator: LinearProgressIndicator
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main8)
-        //WebView
+
+        // Initialize views
         webView = findViewById(R.id.webView)
+        ProgressIndicator = findViewById(R.id.linearProgressIndicator)
+
+        // Configure WebView
         webView.settings.javaScriptEnabled = true
-        webView.webViewClient = WebViewClient()
-        val url = "https://sreeragtv99.github.io/sreeragtv/"
-        webView.loadUrl(url)
+        webView.webViewClient = CustomWebViewClient()
+        webView.webChromeClient = CustomWebChromeClient()
+
+        // Load the URL
+        webView.loadUrl("https://www.arabbank.jo/")
+    }
+
+    inner class CustomWebViewClient : WebViewClient() {
+        override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+            // Show the progress indicator when page starts loading
+            ProgressIndicator.visibility = View.VISIBLE
+            super.onPageStarted(view, url, favicon)
+        }
+
+        override fun onPageFinished(view: WebView?, url: String?) {
+            // Hide the progress indicator when page finishes loading
+            ProgressIndicator.visibility = View.GONE
+            super.onPageFinished(view, url)
+        }
+    }
+
+    inner class CustomWebChromeClient : WebChromeClient() {
+        override fun onProgressChanged(view: WebView?, newProgress: Int) {
+            super.onProgressChanged(view, newProgress)
+
+            // Update progress of the progress indicator
+            ProgressIndicator.progress = newProgress
+        }
 
     }
 }
